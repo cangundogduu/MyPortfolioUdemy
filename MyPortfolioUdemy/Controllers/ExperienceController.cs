71626a685114d6c6fde22e26c85829c9318ad3a2
can gündoğdu <gundogdu.can@hotmail.com>
2025-01-12 23:14:51 +0300

    new things added. 23.13

---
+++ MyPortfolioUdemy/Controllers/ExperienceController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolioUdemy.DAL.Context;
+using MyPortfolioUdemy.DAL.Entities;
+
+namespace MyPortfolioUdemy.Controllers
+{
+	public class ExperienceController : Controller
+	{
+		MyPortfolioContext context=new MyPortfolioContext();
+		public IActionResult ExperienceList()
+		{
+			var values= context.Experiences.ToList();
+			return View(values);
+		}
+
+		[HttpGet]
+		public IActionResult CreateExperience()
+		{
+			return View();
+		}
+
+		public IActionResult CreateExperience(Experience experience)
+		{ 
+			context.Experiences.Add(experience);
+			context.SaveChanges();
+			return RedirectToAction("ExperienceList");
+		}
+	}
+}
